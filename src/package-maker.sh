a8cb1b85ee124411bc72dd522bb85f99d65a75d7
Attila Somogyi <zsolt.attila.somogyi@gmail.com>
2022-03-09 20:55:41 +0100

    feat(core): add summary options to rpm spec

--- src/package-maker.sh
+++ src/package-maker.sh
@@ -3,6 +3,7 @@ package_name=$(basename `git rev-parse --show-toplevel`)
 package_version=$(git describe --abbrev=0 | sed 's/v//')
 arch="noarch"
 license=$(licensee detect $(git rev-parse --show-toplevel) | grep License: | head -1 | sed 's/ //g' | awk -F ':' '{print $2}')
+summary=$(gh repo view --json description | jq -r '.description')
 spec=rpmbuild/SPECS/$package_name.spec
 url=$(git config --get remote.origin.url)
 mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}|| exit 1
@@ -10,7 +11,7 @@ echo -n >$spec
 echo "Name:      $package_name">>$spec
 echo "Version:   $package_version">>$spec
 echo "Release:   1%{?dist}">>$spec
-echo "Summary:   ">>$spec
+echo "Summary:   $summary">>$spec
 echo "License:   $license">>$spec
 echo "URL:       $url" >>$spec
 echo "Source0:   $url" >>$spec
