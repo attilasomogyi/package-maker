c465c85a923c5c140b4f6a8029097b37315ca0b6
Attila Somogyi <zsolt.attila.somogyi@gmail.com>
2022-03-09 20:18:28 +0100

    feat(core): add rpm spec options

--- src/package-maker.sh
+++ src/package-maker.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 package_name=$(basename `git rev-parse --show-toplevel`)
 package_version=$(git describe --abbrev=0 | sed 's/v//')
-license=$(licensee | grep License: | head -1 | sed 's/ //g' | awk -F ':' '{print $2}')
+arch="noarch"
+license=$(licensee detect $(git rev-parse --show-toplevel) | grep License: | head -1 | sed 's/ //g' | awk -F ':' '{print $2}')
 spec=rpmbuild/SPECS/$package_name.spec
+url=$(git config --get remote.origin.url)
 mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}|| exit 1
 echo -n >$spec
-echo "Name:    $package_name">>$spec
-echo "Version: 1%{?dist}">>$spec
-echo "Summary: ">>$spec
-echo "License: $license">>$spec
-echo -n >>$spec
+echo "Name:      $package_name">>$spec
+echo "Version:   $package_version">>$spec
+echo "Release:   1%{?dist}">>$spec
+echo "Summary:   ">>$spec
+echo "License:   $license">>$spec
+echo "URL:       $url" >>$spec
+echo "Source0:   $url" >>$spec
+echo "Patch0:    $url" >>$spec
+echo "BuildArch: $arch">>$spec
