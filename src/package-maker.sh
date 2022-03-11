a28b4ace98afe6bbd922a31b3a200700def78d9e
Attila Somogyi <zsolt.attila.somogyi@gmail.com>
2022-03-11 21:40:00 +0100

    feat(core): add new spec sections to rpm package description

--- src/package-maker.sh
+++ src/package-maker.sh
@@ -6,13 +6,27 @@ license=$(licensee detect $(git rev-parse --show-toplevel) | grep License: | hea
 summary=$(gh repo view --json description | jq -r '.description')
 spec=rpmbuild/SPECS/$package_name.spec
 url=$(gh repo view --json url | jq -r '.url')
+build_requires=""
+requires=""
+source_url=""
 mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}|| exit 1
 echo -n >$spec
-echo "Name:      $package_name">>$spec
-echo "Version:   $package_version">>$spec
-echo "Release:   1%{?dist}">>$spec
-echo "Summary:   $summary">>$spec
-echo "License:   $license">>$spec
-echo "URL:       $url" >>$spec
-echo "Source0:   $url" >>$spec
-echo "BuildArch: $arch">>$spec
+echo "Name:          $package_name">>$spec
+echo "Version:       $package_version">>$spec
+echo "Release:       1%{?dist}">>$spec
+echo "Summary:       $summary">>$spec
+echo "License:       $license">>$spec
+echo "URL:           $url" >>$spec
+echo "Source0:       $source_url" >>$spec
+echo "BuildArch:     $arch">>$spec
+echo "BuildRequires: $build_requires">>$spec
+echo "Requires:      $requires">>$spec
+echo "%description">>$spec
+pandoc -f gfm -t plain $(git rev-parse --show-toplevel)/README.md>>$spec 
+echo "%prep">>$spec
+echo "%build">>$spec
+echo "%install">>$spec
+echo "%check">>$spec
+echo "%files">>$spec
+echo "%changelog">>$spec
+pandoc -f gfm -t plain $(git rev-parse --show-toplevel)/CHANGELOG.md>>$spec
