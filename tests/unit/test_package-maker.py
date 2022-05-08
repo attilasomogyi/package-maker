1171acb975f5605c804ec3005889fe2acfbc3a5a
Attila Somogyi <attila.zsolt.somogyi@gmail.com>
2022-05-08 23:19:59 +0200

    test: add teardown

--- tests/unit/test_package-maker.py
+++ tests/unit/test_package-maker.py
@@ -1,18 +1,23 @@
 from git import Repo
 from src.package_maker.package_maker import Package
-from tempfile import mkdtemp
-import pytest
+from tempfile import TemporaryDirectory
 
 
 def setup_module(module):
     global test_repository_root
-    test_repository_root = str(mkdtemp())
+    global tempdir
+    tempdir = TemporaryDirectory()
+    test_repository_root = tempdir.name
     global test_git_repository
     test_git_repository = Repo.init(test_repository_root)
     global test_git_repository_root_path
     test_git_repository_root_path = test_repository_root+"/.git"
     global test_git_root
     test_git_root = Package(test_git_repository_root_path)
+
+
+def teardown_module(module):
+    tempdir.cleanup()
 
 
 def test_get_git_root():
