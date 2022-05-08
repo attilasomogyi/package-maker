7532836175279d42bbfca1d57b79b16fb4cbe06d
Attila Somogyi <attila.zsolt.somogyi@gmail.com>
2022-05-08 22:23:39 +0200

    test: add new test file

---
+++ tests/unit/test_package-maker.py
@@ -0,0 +1,19 @@
+from git import Repo
+from src.package_maker.package_maker import Package
+from tempfile import mkdtemp
+import pytest
+
+
+def setup_module(module):
+    global test_repository_root
+    test_repository_root = str(mkdtemp())
+    global test_git_repository
+    test_git_repository = Repo.init(test_repository_root)
+    global test_git_repository_root_path
+    test_git_repository_root_path = test_repository_root+"/.git"
+    global test_git_root
+    test_git_root = Package(test_git_repository_root_path)
+
+
+def test_get_git_root():
+    assert test_git_root.get_git_root() == test_repository_root
